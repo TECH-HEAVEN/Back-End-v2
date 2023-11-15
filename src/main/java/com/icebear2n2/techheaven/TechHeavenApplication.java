0b389e145a2ba355bd837fce797f3d6775eb9161
icebear2n2 <icebear2n2@gmail.com>
2023-11-15 10:08:01 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/icebear2n2/techheaven/TechHeavenApplication.java
@@ -0,0 +1,13 @@
+package com.icebear2n2.techheaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechHeavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechHeavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icebear2n2/techheaven/TechHeavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icebear2n2.techheaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechHeavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
